08e1a6aa1fe01b7e6bdb7ac8179c60ae22ded41c
luciosantiagovanderlei <luciosantiagovandelei@gmail.com>
2022-10-11 02:38:40 -0300

    First component

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import NotificationButton from "./components/NotificationButton"
+
+function App() {
+  return(
+    <>
+    <h1>Olá mundo</h1>
+    <NotificationButton/>
+    <NotificationButton/>
+    <NotificationButton/>
+    <NotificationButton/>
+    
+
+    </>
+  )
+}
+
+export default App
