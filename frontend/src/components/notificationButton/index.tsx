e00a9d2ba9cde162427e512707b68042a743507a
luciosantiagovanderlei <luciosantiagovandelei@gmail.com>
2022-10-11 04:10:57 -0300

    Other components

--- frontend/src/components/notificationButton/index.tsx
+++ frontend/src/components/notificationButton/index.tsx
@@ -1,11 +1,11 @@
 
-import icon from '../../assets/img/notification-icon.svg'
+import icone from '../../assets/img/notification-icon.svg'
 import './styles.css'
 function NotificationButton() {
 
     return (
-        <div class="dsmeta-red-btn">
-        <img src={icon} alt="Notificar" />
+        <div className="dsmeta-red-btn">
+        <img src={icone} alt="Notificar" />
       </div>
     )
   }
