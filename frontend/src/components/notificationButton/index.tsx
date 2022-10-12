ebe48107191e837db055af276089b6333ea5299a
luciosantiagovanderlei <luciosantiagovandelei@gmail.com>
2022-10-12 17:52:45 -0300

    Toast

--- frontend/src/components/notificationButton/index.tsx
+++ frontend/src/components/notificationButton/index.tsx
@@ -1,5 +1,6 @@
 
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icone from '../../assets/img/notification-icon.svg'
 import { BASE_URL } from '../../utils/request';
 import './styles.css'
@@ -11,7 +12,7 @@ type Props = {
 function handleClick(id: number){
   axios(`${BASE_URL}/sales/${id}/notification`)
   .then(response => {
-    console.log("Sucesso "  );
+    toast.info("SMS enviado com sucesso");
   } )
 }
 
