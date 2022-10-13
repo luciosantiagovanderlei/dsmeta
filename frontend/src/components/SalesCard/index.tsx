cb6fd229a2a57f54985694883913120451e7419c
luciosantiagovanderlei <luciosantiagovandelei@gmail.com>
2022-10-13 00:50:27 -0300

    fix notification button

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,4 +1,4 @@
-import NotificationButton from '../NotificationButton'
+import NotificationButton from '../notificationButton'
 import './styles.css'
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
